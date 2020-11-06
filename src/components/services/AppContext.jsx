f4361840a99202da565d0dfb3f329eb42c55fe55
dae54 <amethysternest@gmail.com>
2020-11-07 00:37:40 +0300

    remove unwanted commented codes

--- src/components/services/AppContext.jsx
+++ src/components/services/AppContext.jsx
@@ -2,35 +2,11 @@ import React, { useReducer } from "react";
 
 let initialState = {
     breadcrumbPath: []
-
-    // isAuthorized: localStorage.getItem('token') ? true : false,
-    // // isAuth: { status: true, token: localStorage.getItem('token') },
-    // // token: localStorage.getItem('token'),
-    // isLocked: false,
-    // userDetails: JSON.parse(localStorage.getItem('userDetails')) || '',
-    // authIssuedAt: '',
-    // idleTimeDuration: 0
-
-    // budgetName: '', budgetDescription: '', budgetItems: [], budgetId: '',
-    // fetchNew: '', activateBudget: false, startDate: new Date().toLocaleDateString(), endDate: ''
 }
 let reducer = (state, action) => {
     switch (action.type) {
         case 'breadcrumbPath':
             return { ...state, breadcrumbPath: state.breadcrumbPath = action.payload }
-            
-        // case 'token':
-        //     return { ...state, token: state.token = action.payload }
-        // erDetails: state.userDetails = action.payload }
-        // case 'authIssuedAt':case 'isLocked':
-        //     return { ...state, isLocked: state.isLocked = action.payload }
-        // case 'authIssuedAt':
-        //     return { ...state, authIssuedAt: state.authIssuedAt = action.payload }
-        // case 'idleTimeDuration':
-        //     return { ...state, idleTimeDuration: state.idleTimeDuration = action.payload }
-        // case 'userDetails':
-        //     return { ...state, us
-        //     return { ...state, authIssuedAt: state.authIssuedAt.concat(action.payload) }
     }
 }
 
