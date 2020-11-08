14b35763f4d22d4906ca43281b28836647dc21ec
dae54 <amethysternest@gmail.com>
2020-11-08 23:32:15 +0300

    rename class to className

--- src/components/Gadgets/Breadcrumb.jsx
+++ src/components/Gadgets/Breadcrumb.jsx
@@ -10,8 +10,8 @@ export default function Breadcrumb({ routes }) {
     return (
         <React.Fragment>
             <nav aria-label="breadcrumb" className='float-left pt-1'>
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
                         <Link to="" > <i className="fa fa-home"></i> Dashboard</Link>
                     </li>
                     {state.breadcrumbPath.map((route, index) => {
@@ -19,36 +19,36 @@ export default function Breadcrumb({ routes }) {
                             // state.breadcrumbPath[state.breadcrumbPath.length - 1].path === '/' ?
                             // route.active ?
                             state.breadcrumbPath[state.breadcrumbPath.length - 1].url !== route.url ?
-                                <li class="breadcrumb-item">
+                                <li className="breadcrumb-item" key={index}>
                                     <Link to={route.url} >
                                         {route.name}
                                     </Link>
                                 </li>
                                 :
-                                <li class="breadcrumb-item active" aria-current="page">{route.name}</li> 
+                                <li className="breadcrumb-item active" aria-current="page" key={index}>{route.name}</li> 
 
-                            // <li class="breadcrumb-item">
+                            // <li className="breadcrumb-item">
                             //     <Link to="" > <i className="fa fa-home"></i> Home</Link>
                             // </li>
                             // :
-                            // <li class="breadcrumb-item">
+                            // <li className="breadcrumb-item">
                             //     <Link to="" >Home</Link>
                             // </li>
 
 
 
-                            // <li class="breadcrumb-item">
+                            // <li className="breadcrumb-item">
                             //     <Link to="" > <i className="fa fa-home"></i> Home</Link>
                             // </li>
                         )
                     })}
-                    {/* <li class="breadcrumb-item">
+                    {/* <li className="breadcrumb-item">
                         <Link to="" > <i className="fa fa-home"></i> Home</Link>
                     </li>
-                    <li class="breadcrumb-item">
+                    <li className="breadcrumb-item">
                         <Link to="" >Library</Link>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page">Data</li> */}
+                    <li className="breadcrumb-item active" aria-current="page">Data</li> */}
                 </ol>
             </nav>
         </React.Fragment>
