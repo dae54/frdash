a65c895a81b65895445391678edc831880986884
dae54 <amethysternest@gmail.com>
2020-11-27 17:12:46 +0300

    add amazonaws api route

--- src/URL.js
+++ src/URL.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line
-export default URL = 'http://localhost:5000';        //localhost
+// export default URL = 'http://localhost:5000';        //localhost
+export default URL = 'http://ec2-3-12-196-42.us-east-2.compute.amazonaws.com:5000'
 // export default URL = 'http://41.86.177.14:8080';        //localhost
 
 // eslint-disable-next-line
