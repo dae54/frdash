df96150133e962f3794a25e90d178be2a986299f
dae54 <amethysternest@gmail.com>
2020-12-08 10:59:40 +0300

    create custom dashboard widget

--- src/components/Pages/Requests/Statistics.jsx
+++ src/components/Pages/Requests/Statistics.jsx
@@ -27,7 +27,18 @@ export default function Statistics({ statistics }) {
     return (
         <React.Fragment>
             {stats.map((item, index) => {
-                return (<InfoCard key={index} item={item} />)
+                return (
+                    <div key={index} className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-3">
+                        <div className={`dash-widget shadow border-left border-${item.color}`}>
+                            <span className={`dash-widget-bg1 bg-${item.color}`}><i className={`fa fa-${item.icon}`} aria-hidden="true"></i></span>
+                            <div className="dash-widget-info text-right">
+                                <h5 className='pb-2 font-weight-bold'>{item.amount} ({item.count})</h5>
+                                {item.label}
+                            </div>
+                        </div>
+                    </div>
+                    // <InfoCard key={index} item={item} />
+                )
             })}
         </React.Fragment>
     )
