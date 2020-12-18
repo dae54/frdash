2312689b56044979beaf40eecc87155070d782a0
dae54 <amethysternest@gmail.com>
2020-12-19 00:59:41 +0300

    remove some of unwanted context states

--- src/components/Auth/AuthContext.jsx
+++ src/components/Auth/AuthContext.jsx
@@ -2,26 +2,33 @@ import React, { useReducer } from "react";
 let initialState = {
     isAuthorized: localStorage.getItem('token') ? true : false,
     // isAuth: { status: true, token: localStorage.getItem('token') },
-    // token: localStorage.getItem('token'),
+    token: localStorage.getItem('token'),
     isLocked: false,
-    userDetails: '',
+    // userDetails: '',
     authIssuedAt: '',
-    idleTimeDuration: 0
+    idleTimeDuration: 0,
+    authState: '',
+    currentUser: ''
+    /**
+     * authState 'locked', 'authorized', 'unauthorized'
+     */
 }
 let reducer = (state, action) => {
     switch (action.type) {
         case 'isAuthorized':
             return { ...state, isAuthorized: state.isAuthorized = action.payload }
-        // case 'token':
-        //     return { ...state, token: state.token = action.payload }
         case 'isLocked':
             return { ...state, isLocked: state.isLocked = action.payload }
         case 'authIssuedAt':
             return { ...state, authIssuedAt: state.authIssuedAt = action.payload }
         case 'idleTimeDuration':
             return { ...state, idleTimeDuration: state.idleTimeDuration = action.payload }
-        case 'userDetails':
-            return { ...state, userDetails: state.userDetails = action.payload }
+        case 'authState':
+            return { ...state, authState: state.authState = action.payload }
+        case 'currentUser':
+            return { ...state, currentUser: state.currentUser = action.payload }
+        case 'token':
+            return { ...state, token: state.token = action.payload }
         default: return { ...state }
     }
 }
